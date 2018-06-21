450c882c97e9ecc7983120dcfc039fd4bf5ea711
Alex Vyushkov <baguage@gmail.com>
2018-06-21 14:04:06 -0700

    Simulate quantum beats on Rigetti QPU

--- rigetti/quantum_beats_rigetti_qpu.py
+++ rigetti/quantum_beats_rigetti_qpu.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import sys
+import os
 
 from pyquil.quil import Program
 from pyquil.gates import Z, X, H, CNOT, PHASE
@@ -55,14 +57,16 @@ def add_switch_to_singlet_triplet_basis_gate_to_program(program):
 
 
 def main():
-    qvm = QVMConnection()
     agave = get_devices(as_dict=True)['8Q-Agave']
     compiler = CompilerConnection(agave)
-    qpu = QPUConnection(agave)
+    qvm = QVMConnection()  # Perfect QVM
+    qvm_noisy = QVMConnection(agave)  # Simulate Noise
+    qpu = QPUConnection(agave)  # Physical QPU
     print("Timestamp, Singlet (Wavefunction), Triplet (Wavefunction), Singlet (QVM), Triplet (QVM),"
+          "Singlet (QVM Noise), Triplet (QVM Noise), 00 (QVM Noise), 11 (QVM Noise),"
           "Singlet (QPU), Triplet (QPU), 00 (QPU), 11 (QPU)")
     # Rotation
-    for t in range(0, 50):  # ns
+    for t in range(1, 50):  # ns
         p = create_singlet_state()
         add_switch_to_singlet_triplet_basis_gate_to_program(p)
         w_larmor = 0.46  # 4.6e8 1/s as determined in the experiment
@@ -74,28 +78,42 @@ def main():
         p.measure(0, 0)
         p.measure(1, 1)
 
-        print(p)
-        # Run the code on QVM
-        data = qvm.run(p, trials=1000)
+        # Run the code on a perfect QVM (no noise)
+        data = qvm.run(p, trials=1024)
+
+        # simulate physical noise on QVM
+        data_noisy = qvm_noisy.run(p, trials=1000)
+        noisy_data_distr = distribution(data_noisy)
 
         # Run the code on QPU
-        data_qpu = qpu.run(p, trials=1000)
+        # Suppress print statements
+        _old_stdout = sys.stdout
+        with open(os.devnull, 'w') as fp:
+            sys.stdout = fp
+            data_qpu = qpu.run(p, trials=1024)
+        sys.stdout = _old_stdout
 
-        job_id = compiler.compile_async(p)
-        job = compiler.wait_for_job(job_id)
+        # print('compiled quil', job.compiled_quil())
+        # print('gate volume', job.gate_volume())
+        # print('gate depth', job.gate_depth())
+        # print('topological swaps', job.topological_swaps())
+        # print('program fidelity', job.program_fidelity())
+        # print('multiqubit gate depth', job.multiqubit_gate_depth())
 
-        print('compiled quil', job.compiled_quil())
-        print('gate volume', job.gate_volume())
-        print('gate depth', job.gate_depth())
-        print('topological swaps', job.topological_swaps())
-        print('program fidelity', job.program_fidelity())
-        print('multiqubit gate depth', job.multiqubit_gate_depth())
-
-        print("%s, %s, %s, %s, %s, %s, %s, %s, %s" %
+        # Note the order of qubit in Rigetti
+        # http://pyquil.readthedocs.io/en/latest/qvm.html#multi-qubit-basis-enumeration
+        # (1, 0) is singlet, but in string notation it is reversed ('01'), because
+        #
+        #  "The Rigetti QVM enumerates bitstrings such that qubit 0 is the least significant bit (LSB)
+        #   and therefore on the right end of a bitstring"
+        #
+        print("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" %
               (t, probs['01'], probs['10'],
-               distribution(data).get((0, 1), 0), distribution(data).get((1, 0), 0),
-               distribution(data_qpu).get((0, 1), 0), distribution(data_qpu).get((1, 0), 0),
-               distribution(data_qpu).get((1, 1), 0), distribution(data_qpu).get((0, 0), 0))
+               distribution(data).get((1, 0), 0), distribution(data).get((0, 1), 0),
+               noisy_data_distr.get((1, 0), 0), noisy_data_distr.get((0, 1), 0),
+               noisy_data_distr.get((0, 0), 0), noisy_data_distr.get((1, 1), 0),
+               distribution(data_qpu).get((1, 0), 0), distribution(data_qpu).get((0, 1), 0),
+               distribution(data_qpu).get((0, 0), 0), distribution(data_qpu).get((1, 1), 0))
         )
 
 
